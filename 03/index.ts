9979460591effe923d7c60824cffb74dcda769ff
Frederik Born <born@born-solutions.de>
2019-12-10 20:06:40 +0100

    Now both solutions for day 3 work (at least for me)

--- 03/index.ts
+++ 03/index.ts
@@ -1,4 +1,4 @@
-import { map, flatMap, filter, intersectionWith, isEqual } from 'lodash';
+import { map, filter, reduce } from 'lodash';
 import input from './input.txt';
 
 enum Direction {
@@ -13,7 +13,14 @@ interface Instruction {
   steps: number,
 }
 
-type Point = [number, number];
+interface Point {
+  x: number,
+  y: number,
+}
+
+type Segment = [Point, Point];
+
+type Line = Segment[];
 
 function parseInstruction(instruction: string): Instruction {
   const directionChar = instruction.charAt(0);
@@ -25,62 +32,148 @@ function parseInstruction(instruction: string): Instruction {
   });
 }
 
-function* unrollInstruction(start: Point, { direction, steps }: Instruction) {
-  let stepFn: (point: Point) => Point;
-  let buffer: Point = start;
+function applyInstruction(start: Point, { direction, steps }: Instruction): Point {
+  const destination = { x: start.x, y: start.y };
 
-  switch(direction) {
+  switch (direction) {
     case Direction.Up:
-      stepFn = ([x, y]: Point) => [x, y + 1];
+      destination.y += steps;
       break;
     case Direction.Right:
-      stepFn = ([x, y]: Point) => [x + 1, y];
+      destination.x += steps;
       break;
     case Direction.Down:
-      stepFn = ([x, y]: Point) => [x, y - 1];
+      destination.y -= steps;
       break;
     case Direction.Left:
-      stepFn = ([x, y]: Point) => [x - 1, y];
+      destination.x -= steps;
       break;
   }
 
-  for (let i = 0; i < steps; i++) {
-    buffer = stepFn(buffer);
-    yield buffer;
-  }
+
+  return destination;
 }
 
-function getDistance([aX, aY]: Point, [bX, bY]: Point): number {
-  return Math.abs(aX - bX) + Math.abs(aY - bY);
+function createLine(start: Point, instructions: Instruction[]): Line {
+  const seededLine: Segment[] =  reduce(instructions, (acc, instruction) => {
+    const start = acc[acc.length - 1][1];
+
+    return [...acc, [start, applyInstruction(start, instruction)]];
+  }, [[start, start]]);
+
+  return seededLine.splice(1);
 }
+
+
+function getDistance(a: Point, b: Point): number {
+  return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+}
+
+// line intercept math by Paul Bourke http://paulbourke.net/geometry/pointlineplane/
+// Determine the intersection point of two line segments
+// Return FALSE if the lines don't intersect
+function intersect([a, b]: Segment, [c, d]: Segment): Point | false {
+
+  // Check if none of the lines are of length 0
+	if ((a.x === b.x && a.y === b.y) || (c.x === d.x && c.y === d.y)) {
+		return false;
+	}
+
+	const denominator = ((d.y - c.y) * (b.x - a.x) - (d.x - c.x) * (b.y - a.y));
+
+  // Lines are parallel
+	if (denominator === 0) {
+		return false;
+	}
+
+	const ua = ((d.x - c.x) * (a.y - c.y) - (d.y - c.y) * (a.x - c.x)) / denominator;
+	const ub = ((b.x - a.x) * (a.y - c.y) - (b.y - a.y) * (a.x - c.x)) / denominator;
+
+  // is the intersection along the segments
+	if (ua < 0 || ua > 1 || ub < 0 || ub > 1) {
+		return false;
+	}
+
+  // Return a object with the x and y coordinates of the intersection
+	let x = a.x + ua * (b.x - a.x);
+	let y = a.y + ua * (b.y - a.y);
+
+	return { x, y } ;
+}
+
+function findIntersections(lineA: Line, lineB: Line) {
+  let buffer: Point[] = [];
+
+  for (const segmentA of lineA) {
+    for (const segmentB of lineB) {
+      const intersection = intersect(segmentA, segmentB);
+      if (intersection) {
+        buffer = [...buffer, intersection];
+      }
+    }
+  }
+
+  return filter(buffer, ({ x, y }) => !(x == 0 && y == 0));
+}
+
+function countStepsToPoint(from: Point, to: Point, instructions: Instruction[]): number {
+  let current = Object.assign({}, from);
+  let totalSteps = 0;
+
+  for (const { direction, steps } of instructions) {
+    for (let step = 0; step < steps; step++) {
+
+      if (current.x == to.x && current.y == to.y) {
+        return totalSteps;
+      }
+
+      switch (direction) {
+        case Direction.Up:
+          current.y += 1;
+          break;
+        case Direction.Right:
+          current.x += 1;
+          break;
+        case Direction.Down:
+          current.y -= 1;
+          break;
+        case Direction.Left:
+          current.x -= 1;
+          break;
+      }
+
+      totalSteps += 1;
+    }
+  }
+
+  return NaN;
+}
+
+// Part one
 
 // Kinda arbitrary
-const centralPort: Point = [0, 0];
+const centralPort = { x: 0, y: 0 };
 
 const inputLines = filter(input.split('\n'), line => line.length != 0);
-
 const parsedWires = map(inputLines, line => {
   const instructions = filter(line.split(','), instruction => instruction.length != 0);
 
   return map(instructions, parseInstruction);
 });
 
-const interpolatedPoints = map(parsedWires, instructions => {
-  let start = centralPort;
+const lines = map(parsedWires, wire => createLine(centralPort, wire));
+const intersections = findIntersections(lines[0], lines[1]);
+const distances = map(intersections, point => getDistance(centralPort, point));
 
-  return flatMap(instructions, instruction => {
-    const points = [...unrollInstruction(start, instruction)];
-    start = points[points.length - 1];
+const output1 = [...distances].sort((a, b) => a - b)[0];
 
-    return points;
-  })
-});
+// Part two
 
-const intersections = intersectionWith(...interpolatedPoints, <any>isEqual);
-const distances = map(intersections, (intersection: Point) => getDistance(centralPort, intersection));
+const firstIntersection = intersections[0];
+const lineASteps = countStepsToPoint(centralPort, firstIntersection, parsedWires[0]);
+const lineBSteps = countStepsToPoint(centralPort, firstIntersection, parsedWires[1]);
 
-const output1 = distances.sort()[0];
-const output2 = 0;
+const output2 = lineASteps + lineBSteps;
 
 export {
   output1 as solutionPartOne,
